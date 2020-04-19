94d12e28aa96cd9d6f960375d34a08866ddbc2e3
codeanticode <andres.colubri@gmail.com>
2020-04-19 15:20:38 -0400

    add all steps to makeall script

--- makeall.sh
+++ makeall.sh
@@ -7,6 +7,16 @@ last_year1=$((last_year - 1))
 for (( year0=$first_year; year0<=$last_year1; year0+=2 ))
 do
   year1=$((year0 + 1));
-#   python getdata.py $year0-$year1
+  python getdata.py $year0-$year1
   python makedataset.py $year0-$year1
 done
+
+python mergedatasets.py $last_year-$last_year
+
+for (( year0=$first_year; year0<=$last_year1; year0+=2 ))
+do
+  year1=$((year0 + 1));
+  python finaldataset.py $year0-$year1
+done
+
+python finaldataset.py $last_year-$last_year
